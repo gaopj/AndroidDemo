bb5148cdd73a6a5383938e7097d684dceda32045
Piji Gao <gaopj@pku.edu.cn>
2018-04-30 16:25:57 +0800

    WindowManagerDemo done

---
+++ WindowManagerDemo/app/src/main/java/com/gpj/windowmanagerdemo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.gpj.windowmanagerdemo;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Handler mHandler = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button =  findViewById(R.id.button);
+        requestDrawOverLays();
+        mHandler = new Handler();
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                mHandler.postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        WindowUtils.showPopupWindow(getApplication());
+
+                    }
+                }, 1000 * 3);
+
+            }
+        });
+    }
+
+    public static int OVERLAY_PERMISSION_REQ_CODE = 1234;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void requestDrawOverLays() {
+        if (!Settings.canDrawOverlays(MainActivity.this)) {
+            Toast.makeText(this, "can not DrawOverlays", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + MainActivity.this.getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+        } else {
+            // Already hold the SYSTEM_ALERT_WINDOW permission, do addview or something.
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                // SYSTEM_ALERT_WINDOW permission not granted...
+                Toast.makeText(this, "Permission Denieddd by user.Please Check it in Settings", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Allowed", Toast.LENGTH_SHORT).show();
+                // Already hold the SYSTEM_ALERT_WINDOW permission, do addview or something.
+            }
+        }
+    }
+}
